8d0f3bed4b080863b08fe073e0a25c9c4450653e
Olamide Oladinni <olajide1987@gmail.com>
2020-02-29 11:43:49 -0600

    add event listener

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -1,10 +1,21 @@
 import tank from './Components/tank.js';
 import graveyard from './Components/graveyard.js';
+import personData from './helpers/data/personData.js'
+
+
+const sharkAttackEvent = () => {
+    personData.randomMurder();
+    tank.tankBuilder();
+    graveyard.buildGraveyard();
+
+
+}
 
 
 const init = () => {
     tank.tankBuilder();
     graveyard.buildGraveyard();
+    $('#bite-me').click(sharkAttackEvent);
 };
 
 init();
